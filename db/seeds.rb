4d700dadc15215809c8c1c34febd80f709f548ff
Edward Iga Kigongo <igakigongo@gmail.com>
2020-10-08 17:06:03 +0200

    add database seed for doctors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+doctor_specializations = %w[
+  Allergist
+  Anesthesiologists
+  Cardiologist
+  Dermatologist
+  Endoctrinologist
+  Gastroenterologist
+  Nephrologist
+  Neurologist
+  Obstetrician
+  Oncologist
+  Ophthalmologist
+  Otolaryngologist
+  Psychiatrist
+  Pulmonologist
+  Radiologist
+  Rheumatologist
+  Urologist
+]
+
+doctor_specializations.each do |specialization|
+  Doctor.create!(name: Faker::Name.name, speciality: specialization)
+end
